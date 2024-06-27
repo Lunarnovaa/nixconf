2f0011978d4d1052abc3588fcfaafff36aad742b
lunarnovaa <auraboreal@proton.me>
2024-06-27 12:54:30 -0700

    test

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -37,9 +37,6 @@
     shell = pkgs.zsh;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

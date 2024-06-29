40442fccce70a6337f1c08c0c2c40dcbe3b3018e
lunarnovaa <auraboreal@proton.me>
2024-06-28 18:22:19 -0700

    Switch to Lightmode

--- nixosModules/bundles/stylix/stylix.nix
+++ nixosModules/bundles/stylix/stylix.nix
@@ -7,26 +7,27 @@
   stylix = {
     enable = true;
 
-    base16Scheme = {
-      base00 = "171B24";
-      base01 = "1F2430";
-      base02 = "242936";
-      base03 = "707A8C";
-      base04 = "8A9199";
-      base05 = "CCCAC2";
-      base06 = "D9D7CE";
-      base07 = "F3F4F5";
-      base08 = "F28779";
-      base09 = "FFAD66";
-      base0A = "FFD173";
-      base0B = "D5FF80";
-      base0C = "95E6CB";
-      base0D = "5CCFE6";
-      base0E = "D4BFFF";
-      base0F = "F29E74";
-    };
+   # base16Scheme = {
+   #   base00 = "171B24";
+   #   base01 = "1F2430";
+   #   base02 = "242936";
+   #   base03 = "707A8C";
+   #   base04 = "8A9199";
+   #   base05 = "CCCAC2";
+   #   base06 = "D9D7CE";
+   #   base07 = "F3F4F5";
+   #   base08 = "F28779";
+   #   base09 = "FFAD66";
+   #   base0A = "FFD173";
+   #   base0B = "D5FF80";
+   #   base0C = "95E6CB";
+   #   base0D = "5CCFE6";
+   #   base0E = "D4BFFF";
+   #   base0F = "F29E74";
+   # };
 
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/github.yaml";
+    #https://tinted-theming.github.io/base16-gallery/
     image = ./andre-benz.jpg;
 
     cursor.package = pkgs.bibata-cursors;

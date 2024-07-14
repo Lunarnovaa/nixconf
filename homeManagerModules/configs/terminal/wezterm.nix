b69c9cbfd7d8bb59ae501098930b19fb6903e44d
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-07-13 21:24:41 -0700

    Wezterm Config

--- homeManagerModules/configs/terminal/wezterm.nix
+++ homeManagerModules/configs/terminal/wezterm.nix
@@ -8,10 +8,17 @@
     programs.wezterm = {
       enable = true;
       enableZshIntegration = true;
-      extraConfig = ''        return {
-              font = wezterm.font 'Ubuntu Mono'
+      extraConfig = ''
+        return {
+          font = wezterm.font 'Ubuntu Mono',
 
-             } '';
+          hide_tab_bar_if_only_one_tab = true,
+          prefer_to_spawn_tabs = true,
+
+          initial_cols = 98,
+          initial_rows = 26,
+        }
+      '';
     };
   };
 }

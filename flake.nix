0e8b2507c0c0200b8d26b6d5e599ca6c7236d61e
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-07-18 15:50:14 -0700

    added disabled nixos-hardware flake

--- flake.nix
+++ flake.nix
@@ -20,6 +20,8 @@
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = {
@@ -51,6 +53,7 @@
           inputs.stylix.nixosModules.stylix
           #inputs.xremap-flake.nixosModules.default
           inputs.lix-module.nixosModules.default
+          # inputs.nixos-hardware.nixosModules.framework-13-7040-amd #having issues
         ];
       };
     };

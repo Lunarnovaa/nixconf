8ec0fa2979a900116c3315873ee5766dc014f331
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-07-19 17:17:37 -0700

    added pinyin support through fcitx5; removed xserver keyboard control

--- nixosModules/bundles/general-desktop.nix
+++ nixosModules/bundles/general-desktop.nix
@@ -8,9 +8,16 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "colemak";
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      rime-data
+      fcitx5-rime
+      fcitx5-gtk
+      fcitx5-configtool #if having issues with qt compatibility, run fcitx5-config-qt
+      fcitx5-chinese-addons
+      fcitx5-mozc
+    ];
   };
 
   networking.networkmanager.enable = true; #enable network

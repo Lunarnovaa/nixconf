6bd2402abb875c78f75d23eca8cd27186a0f03d3
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-07-29 10:13:55 -0700

    Flake Update; DuckDuckGo default search engine

--- homeManagerModules/configs/firefox/firefox.nix
+++ homeManagerModules/configs/firefox/firefox.nix
@@ -16,43 +16,50 @@
   programs.firefox = {
     enable = true;
     profiles.lunarnova = {
-      search.force = true;
       isDefault = true;
+
       settings = {};
 
-      search.engines = {
-        "Nix Packages" = {
-          urls = [
-            {
-              template = "https://search.nixos.org/packages";
-              params = [
-                {
-                  name = "type";
-                  value = "packages";
-                }
-                {
-                  name = "query";
-                  value = "{searchTerms}";
-                }
-              ];
-            }
-          ];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = ["@np"];
-        };
+      search = {
+        force = true;
+        #Default Search Engine
+        default = "DuckDuckGo";
 
-        "NixOS Wiki" = {
-          urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          updateInterval = 24 * 60 * 60 * 1000; # every day
-          definedAliases = ["@nw"];
-        };
+        # Custom Search Engines
+        engines = {
+          "Nix Packages" = {
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = ["@np"];
+          };
 
-        "Home Manager Options" = {
-          urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";}];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          updateInterval = 24 * 60 * 60 * 1000; # every day
-          definedAliases = ["@hmo"];
+          "NixOS Wiki" = {
+            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@nw"];
+          };
+
+          "Home Manager Options" = {
+            urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";}];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = ["@hmo"];
+          };
         };
       };
     };

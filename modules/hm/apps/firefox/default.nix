9d3b693153df3f3ba4b25a28d3197b0aeea522b5
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-10-20 11:45:40 -0700

    firefox changes
    added nix options search engine
    refactored search engines out of default.nix

--- modules/hm/apps/firefox/default.nix
+++ modules/hm/apps/firefox/default.nix
@@ -4,15 +4,9 @@
   config,
   pkgs,
   ...
-}: let
-  pdbImageLink = "https://www.protondb.com/sites/protondb/images/site-logo.svg";
-
-  pdbImage = pkgs.fetchurl {
-    url = pdbImageLink;
-    sha256 = "1q19x63dhjbsjc1nj58cv6w3ag5ciipv15g9h1hdm6mjf2db9fja";
-  };
-in {
+}: {
   imports = [
+    ./search-engines.nix
     ./vertical-tabs.nix
   ];
 
@@ -22,58 +16,10 @@ in {
       isDefault = true;
 
       search = {
+        # Enable search configuration
         force = true;
         #Default Search Engine
         default = "DuckDuckGo";
-
-        # Custom Search Engines
-        engines = {
-          "Nix Packages" = {
-            urls = [
-              {
-                template = "https://search.nixos.org/packages";
-                params = [
-                  {
-                    name = "type";
-                    value = "packages";
-                  }
-                  {
-                    name = "query";
-                    value = "{searchTerms}";
-                  }
-                ];
-              }
-            ];
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = ["@np"];
-          };
-
-          "NixOS Wiki" = {
-            urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@nw"];
-          };
-
-          "Home Manager Options" = {
-            urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";}];
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@hmo"];
-          };
-
-          "ProtonDB" = {
-            urls = [{template = "https://www.protondb.com/search?q={searchTerms}";}];
-            icon = "${pdbImage}";
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@pdb"];
-          };
-          "Rate My Professor" = {
-            urls = [{template = "https://www.ratemyprofessors.com/search/professors/1506?q={searchTerms}";}];
-            updateInterval = 24 * 60 * 60 * 1000; # every day
-            definedAliases = ["@rmp"];
-          };
-        };
       };
     };
   };
---
+++ modules/hm/apps/firefox/search-engines.nix
@@ -0,0 +1,80 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  pdbImageLink = "https://www.protondb.com/sites/protondb/images/site-logo.svg";
+
+  pdbImage = pkgs.fetchurl {
+    url = pdbImageLink;
+    sha256 = "1q19x63dhjbsjc1nj58cv6w3ag5ciipv15g9h1hdm6mjf2db9fja";
+  };
+in {
+  programs.firefox.profiles.lunarnova.search.engines = {
+    "Nix Packages" = {
+      urls = [
+        {
+          template = "https://search.nixos.org/packages";
+          params = [
+            {
+              name = "type";
+              value = "packages";
+            }
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      definedAliases = ["@np"];
+    };
+    "Nix Options" = {
+      urls = [
+        {
+          template = "https://search.nixos.org/options";
+          params = [
+            {
+              name = "type";
+              value = "packages";
+            }
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      definedAliases = ["@no"];
+    };
+    "NixOS Wiki" = {
+      urls = [{template = "https://wiki.nixos.org/w/index.php?search={searchTerms}";}];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      updateInterval = 24 * 60 * 60 * 1000; # every day
+      definedAliases = ["@nw"];
+    };
+
+    "Home Manager Options" = {
+      urls = [{template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=master";}];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      updateInterval = 24 * 60 * 60 * 1000; # every day
+      definedAliases = ["@hmo"];
+    };
+
+    "ProtonDB" = {
+      urls = [{template = "https://www.protondb.com/search?q={searchTerms}";}];
+      icon = "${pdbImage}";
+      updateInterval = 24 * 60 * 60 * 1000; # every day
+      definedAliases = ["@pdb"];
+    };
+    "Rate My Professor" = {
+      urls = [{template = "https://www.ratemyprofessors.com/search/professors/1506?q={searchTerms}";}];
+      updateInterval = 24 * 60 * 60 * 1000; # every day
+      definedAliases = ["@rmp"];
+    };
+  };
+}

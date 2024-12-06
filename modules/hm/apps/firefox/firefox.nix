0b958eb2af06a07ed18414082129362fec1c6429
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2024-12-05 19:44:23 -0800

    enabled tab groups for non-vertical tabs firefox

--- modules/hm/apps/firefox/firefox.nix
+++ modules/hm/apps/firefox/firefox.nix
@@ -19,13 +19,11 @@ in {
         #Default Search Engine
         default = "DuckDuckGo";
       };
-
-      /*
-         uncomment when updating to firefox 133.0
-      settings = mkIf (!config.firefox-vertical-tabs.enable) {
+      
+      settings = mkIf (! config.firefox-vertical-tabs.enable) {
         "browser.tabs.groups.enable" = true;
       };
-      */
+      
     };
   };
 }

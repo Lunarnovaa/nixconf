188f85401c0dc1957c4aa6b0059d15281f358afe
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-23 10:47:58 -0800

    spotify-player: migrate -> hjr

--- modules/programs/spotify/spotify-player.nix
+++ modules/programs/spotify/spotify-player.nix
@@ -1,30 +1,32 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
+
+  inherit (lib.modules) mkDefault;
   catppuccin-spotify-player = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "spotify-player";
     rev = "34b3d23806770185b72466d777853c73454b85a6";
     hash = "sha256-eenf1jB8b2s2qeG7wAApGwkjJZWVNzQj/wEZMUgnn5U=";
   };
-  toTOML = (pkgs.formats.toml {}).generate;
 in {
   hjem.users.lunarnova = {
-    packages = with pkgs; [spotify-player];
-    files = {
-      ".config/spotify-player/app.toml".source = toTOML "spotify-player config" {
+    rum.programs.spotify-player = {
+      enable = true;
+      settings = { 
         client_id_command = "cat ${config.age.secrets.spotifyClientID.path}";
+        theme = "Catppuccin-mocha";
         device = {
           name = "${config.networking.hostName}";
           device_type = "computer";
           volume = 40;
           normalization = true;
-          theme = "Catppuccin-macchiato";
         };
       };
-      ".config/spotify-player/theme.toml".source = "${catppuccin-spotify-player}/theme.toml";
     };
+    files.".config/spotify-player/theme.toml".source = mkDefault "${catppuccin-spotify-player}/theme.toml";
   };
 }

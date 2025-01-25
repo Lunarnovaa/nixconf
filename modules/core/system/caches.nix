677f33639ee97fdf25f9140ab89904e9c1437f78
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-01-25 13:16:30 -0800

    cosmic: ACTUALLY remove cosmic for real this time (caches)

--- modules/core/system/caches.nix
+++ modules/core/system/caches.nix
@@ -1,9 +1,5 @@
 {...}: let
   inherit (builtins) concatLists;
-  cosmic = {
-    substituters = ["https://cosmic.cachix.org/"];
-    trusted-public-keys = ["cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="];
-  };
   hyprland = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
@@ -12,11 +8,9 @@ in {
   # Enable caches to avoid building from source
   nix.settings = {
     substituters = concatLists [
-      cosmic.substituters
       hyprland.substituters
     ];
     trusted-public-keys = concatLists [
-      cosmic.trusted-public-keys
       hyprland.trusted-public-keys
     ];
   };

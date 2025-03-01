bd9d3e1df8624f43a9de4763f7a83fed0f911671
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-03-01 11:53:00 -0800

    update vesktop theme

--- modules/profiles/gaming/programs/vesktop.nix
+++ modules/profiles/gaming/programs/vesktop.nix
@@ -9,8 +9,8 @@
   # Referenced from NotAShelf/nyx, fetching css file directly as opposed to importing the import of this style sheet :)
   catppuccin-mocha-css = pkgs.fetchurl {
     url = "https://catppuccin.github.io/discord/dist/catppuccin-mocha.theme.css";
-    sha256 = "0vnvzb8gb1dk9ld80mik1avvqbiklbkd6r88gqpfcx779y8dcffi";
-  };
+    sha256 = "1mpmzkpn13hbj880wbpr0iqhhazryav2rfvy8d5gbziv6h475sck";
+  }; 
 in {
   config = mkIf gaming.apps.discord {
     hjem.users.lunarnova = {

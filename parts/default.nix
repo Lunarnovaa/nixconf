e050f3c2f4138a7fe3659ff0a48bd8e811bea563
lunarnovaa <35857948+Lunarnovaa@users.noreply.github.com>
2025-03-05 19:29:30 -0800

    parts/: more black magic
    manually importing a lib function for basically no reason other than reducing the number of default.nix's. while this is really poor practice, i am doing it anyway until i find a problem. yes this is dumb, no i don't care, this is my config, for me to fuck around and find out.

--- parts/default.nix
+++ parts/default.nix
@@ -1,13 +1,26 @@
-{
-  imports = [
-    ./apps # Special derivations to be run with 'nix run'
+{lib, ...}: let 
+  inherit (lib.lists) concatLists;
 
-    ./lib # My extended-lib functions
+  # pls ignore this unless you understand the black magic :)
+  # basically im just importing a function from my lib manually
+  # because at this stage of the flake, my lib has not been extended
+  # do NOT try this at home - frankly, i shouldn't be.
+  # sometimes i just like to do awful things in my flake for fun
+  listNixRecursive = import ./lib/lunar/importers/listNixRecursive.nix {inherit lib;};
+in {
+  imports = concatLists [
+    # Import recursively the ol' fashioned way
+    (listNixRecursive ./apps) # Special derivations to be run with 'nix run'
+    
+    (listNixRecursive ./modules) # Special modules designed to be included in specialArgs
+    
+    (listNixRecursive ./misc) # Miscellaneous flake settings
+    
+    # Import only ./default.nix which then uses other means to import
+    [
+      ./lib # My extended-lib functions
 
-    ./modules # Special modules designed to be included in specialArgs
-
-    ./pkgs # My own packages
-
-    ./misc # Miscellaneous flake settings
+      ./pkgs # My own packages
+    ]
   ];
 }
--- parts/lib/lunar/importers/listNixRecursive.nix
+++ parts/lib/lunar/importers/listNixRecursive.nix
@@ -1,8 +1,28 @@
 libArgs: let
   inherit (libArgs.lib.trivial) pipe;
-  inherit (libArgs.lib.strings) hasSuffix;
-  inherit (libArgs.lib.lunar.importers) listFilesRecursiveClean;
-  inherit (builtins) filter;
+  inherit (libArgs.lib.strings) hasSuffix hasPrefix;
+  inherit (libArgs.lib.lists) flatten;
+  inherit (libArgs.lib.attrsets) mapAttrsToList filterAttrs;
+  inherit (builtins) filter readDir;
+
+  # taken from nixpkgs and modified by me so that any directories
+  # with the prefix _ would not have their files imported.
+  # yes, i decided to rewrite it as a pipe function.
+  # please note you can find the original function under
+  # lib.filesystem.listFilesRecursive
+  listFilesRecursiveClean = path:
+    pipe path [
+      readDir
+      (filterAttrs (n: _: (!hasPrefix "_" n))) # this is the one part i added to the function
+      (mapAttrsToList (
+        name: type:
+          if type == "directory"
+          then listFilesRecursiveClean (path + "/${name}")
+          else path + "/${name}"
+      ))
+      flatten
+    ];
+
 in
   # This simple function just lists any nix files that are not the special
   # 'module.nix' file. Previously this was split into an "importNix" that
